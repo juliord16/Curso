d3d2e7992c7ffa49a1cfc7a112c63a99fbb7393b
Ruiz Diaz Julio <juliord16@gmail.com>
2023-02-03 17:55:32 -0300

    entrega cv

---
+++ js/cvJs.js
@@ -0,0 +1,12 @@
+
+
+
+document.getElementById('btnin').addEventListener ("click", function(){
+    console.log("casamos el evento del click linkind")
+    location.href = 'https://www.linkedin.com/in/julio-ruiz-diaz-a50504b3/';
+});
+
+document.getElementById('btngit').addEventListener ("click", function(){
+    console.log("casamos el evento del click github")
+    location.href = 'https://github.com/juliord16';
+});
